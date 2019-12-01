7437c181ef65875b9946ef874ce6cef720dc566e
Eugene Berezin <berezin.nlp@gmail.com>
2019-12-01 15:47:14 -0800

    Dummy collectionView for places

--- FunMaps/FunMaps/Main/MainController.swift
+++ FunMaps/FunMaps/Main/MainController.swift
@@ -6,38 +6,49 @@
 //  Copyright © 2019 Eugene Berezin. All rights reserved.
 //
 
-import Foundation
-import SwiftUI
+import UIKit
 import MapKit
 import LBTATools
 
 extension MainController: MKMapViewDelegate {
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
         let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
         annotationView.canShowCallout = true
+//        annotationView.image = #imageLiteral(resourceName: "tourist")
         return annotationView
     }
+    
 }
 
-
-
 class MainController: UIViewController {
+    
     let mapView = MKMapView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
         view.addSubview(mapView)
         mapView.fillSuperview()
-
+        
         setupRegionForMap()
         
-        //setupAnnotationsForMap()
+//        setupAnnotationsForMap()
         performLocalSearch()
         setupSearchUI()
+        setupLocationsCarousel()
     }
     
+    let locationsController = LocationsCarouselController(scrollDirection: .horizontal)
+    
+    fileprivate func setupLocationsCarousel() {
+        let locationsView = locationsController.view!
+            
+        view.addSubview(locationsView)
+        locationsView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, size: .init(width: 0, height: 150))
+    }
     
     let searchTextField = UITextField(placeholder: "Search query")
     
@@ -45,20 +56,22 @@ class MainController: UIViewController {
         let whiteContainer = UIView(backgroundColor: .white)
         view.addSubview(whiteContainer)
         whiteContainer.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        
         whiteContainer.stack(searchTextField).withMargins(.allSides(16))
         
-              // listen for text changes and then perform new search
-                // OLD SCHOOL
-        //        searchTextField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
+        // listen for text changes and then perform new search
+        // OLD SCHOOL
+//        searchTextField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
         
-        NotificationCenter.default
+        
+        // NEW SCHOOL Search Throttling
+        // search on the last keystroke of text changes and basically wait 500 milliseconds
+        _ = NotificationCenter.default
             .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
-            .debounce(for: .microseconds(500), scheduler: RunLoop.main)
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
             .sink { (_) in
                 self.performLocalSearch()
         }
-        
-        
     }
     
     @objc fileprivate func handleSearchChanges() {
@@ -95,28 +108,72 @@ class MainController: UIViewController {
     
     fileprivate func setupAnnotationsForMap() {
         let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.769795, longitude: -122.414490)
-        annotation.title = "San Francisco!"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        annotation.title = "San Francisco"
         annotation.subtitle = "CA"
         mapView.addAnnotation(annotation)
         
         let appleCampusAnnotation = MKPointAnnotation()
         appleCampusAnnotation.coordinate = .init(latitude: 37.3326, longitude: -122.030024)
-        appleCampusAnnotation.title = "Apple Campus! Dream Job"
-        appleCampusAnnotation.subtitle = "Cupertiono CA"
+        appleCampusAnnotation.title = "Apple Campus"
+        appleCampusAnnotation.subtitle = "Cupertino, CA"
         mapView.addAnnotation(appleCampusAnnotation)
         
         mapView.showAnnotations(self.mapView.annotations, animated: true)
-        
     }
     
     fileprivate func setupRegionForMap() {
-        let coordinate = CLLocationCoordinate2D(latitude: 37.769795, longitude: -122.414490)
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
         mapView.setRegion(region, animated: true)
     }
 }
+
+// SwiftUI Preview
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> MainController {
+            return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainController, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+            
+        }
+        
+        typealias UIViewControllerType = MainController
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 extension MKMapItem {
     func address() -> String {
@@ -142,31 +199,4 @@ extension MKMapItem {
         return addressString
     }
 }
-
-
-
-
-//SwiftUI Preview
-
-struct MainPreview: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-    
-    struct ContentView: UIViewControllerRepresentable {
-        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContentView>) -> MainController {
-            return MainController()
-        }
-        
-        func updateUIViewController(_ uiViewController: MainController, context: UIViewControllerRepresentableContext<MainPreview.ContentView>) {
-            
-        }
-        
-        
-        typealias UIViewControllerType = MainController
-        
-        
-    }
-}
-
 
