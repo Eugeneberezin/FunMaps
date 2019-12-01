ba386ce2edf15c435ba4883fbeb7f05fd39f219f
Eugene Berezin <berezin.nlp@gmail.com>
2019-12-01 09:51:00 -0800

    searchField.

--- FunMaps/FunMaps/Main/MainController.swift
+++ FunMaps/FunMaps/Main/MainController.swift
@@ -35,58 +35,63 @@ class MainController: UIViewController {
         
         //setupAnnotationsForMap()
         performLocalSearch()
-        
+        setupSearchUI()
     }
     
     
-    fileprivate func performLocalSearch() {
-            let request = MKLocalSearch.Request()
-            request.naturalLanguageQuery = "Apple"
-            request.region = mapView.region
-            
-            let localSearch = MKLocalSearch(request: request)
-            localSearch.start { (resp, err) in
-                if let err = err {
-                    print("Failed local search:", err)
-                    return
-                }
-                
-                // Success
-                resp?.mapItems.forEach({ (mapItem) in
-                    
-    //                print(mapItem.placemark.subThoroughfare ?? "")
-                    
-                    let placemark = mapItem.placemark
-                    var addressString = ""
-                    if placemark.subThoroughfare != nil {
-                        addressString = placemark.subThoroughfare! + " "
-                    }
-                    if placemark.thoroughfare != nil {
-                        addressString += placemark.thoroughfare! + ", "
-                    }
-                    if placemark.postalCode != nil {
-                        addressString += placemark.postalCode! + " "
-                    }
-                    if placemark.locality != nil {
-                        addressString += placemark.locality! + ", "
-                    }
-                    if placemark.administrativeArea != nil {
-                        addressString += placemark.administrativeArea! + " "
-                    }
-                    if placemark.country != nil {
-                        addressString += placemark.country!
-                    }
-                    print(addressString)
-                    
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = mapItem.placemark.coordinate
-                    annotation.title = mapItem.name
-                    self.mapView.addAnnotation(annotation)
-    //                mapItem.placemark.coordinate
-                })
-                self.mapView.showAnnotations(self.mapView.annotations, animated: true)
-            }
+    let searchTextField = UITextField(placeholder: "Search query")
+    
+    fileprivate func setupSearchUI() {
+        let whiteContainer = UIView(backgroundColor: .white)
+        view.addSubview(whiteContainer)
+        whiteContainer.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        whiteContainer.stack(searchTextField).withMargins(.allSides(16))
+        
+              // listen for text changes and then perform new search
+                // OLD SCHOOL
+        //        searchTextField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
+        
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
+            .debounce(for: .microseconds(500), scheduler: RunLoop.main)
+            .sink { (_) in
+                self.performLocalSearch()
         }
+        
+        
+    }
+    
+    @objc fileprivate func handleSearchChanges() {
+        performLocalSearch()
+    }
+    
+    fileprivate func performLocalSearch() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchTextField.text
+        request.region = mapView.region
+        
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.start { (resp, err) in
+            if let err = err {
+                print("Failed local search:", err)
+                return
+            }
+            
+            // Success
+            // remove old annotations
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
+            resp?.mapItems.forEach({ (mapItem) in
+                print(mapItem.address())
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            })
+            self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+        }
+    }
     
     fileprivate func setupAnnotationsForMap() {
         let annotation = MKPointAnnotation()
@@ -112,6 +117,32 @@ class MainController: UIViewController {
         mapView.setRegion(region, animated: true)
     }
 }
+
+extension MKMapItem {
+    func address() -> String {
+        var addressString = ""
+        if placemark.subThoroughfare != nil {
+            addressString = placemark.subThoroughfare! + " "
+        }
+        if placemark.thoroughfare != nil {
+            addressString += placemark.thoroughfare! + ", "
+        }
+        if placemark.postalCode != nil {
+            addressString += placemark.postalCode! + " "
+        }
+        if placemark.locality != nil {
+            addressString += placemark.locality! + ", "
+        }
+        if placemark.administrativeArea != nil {
+            addressString += placemark.administrativeArea! + " "
+        }
+        if placemark.country != nil {
+            addressString += placemark.country!
+        }
+        return addressString
+    }
+}
+
 
 
 
